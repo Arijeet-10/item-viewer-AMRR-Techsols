16c23900b04d750abe72441edf6243750ec14121
Arijeet Das <arijeetdas888@gmail.com>
2025-06-21 05:28:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,30 @@
-
+// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAvI1dJBATlnbZizkPc_pjZXHq1cyx9azg",
+  authDomain: "whatsapp-clone-83920.firebaseapp.com",
+  projectId: "whatsapp-clone-83920",
+  storageBucket: "whatsapp-clone-83920.firebasestorage.app",
+  messagingSenderId: "797443983586",
+  appId: "1:797443983586:web:7da162ef01d56ddd0436bb",
+  measurementId: "G-8PXQ25XN5T"
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+if (typeof window !== 'undefined') {
+  // Enable analytics only in the browser
+  getAnalytics(app);
+}
 
 export { db };
